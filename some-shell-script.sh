c8f2f8214bc6869afb5ee57f9a670487e217d122
Rusab Naeem Khan <rusab.khan@lyftrondata.com>
2021-10-04 21:49:59 +0500

    added bashrc

--- some-shell-script.sh
+++ some-shell-script.sh
@@ -6,9 +6,10 @@ else
     wget https://downloads.apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop2.7.tgz
     tar xvf spark-*
     sudo mv spark-3.0.1-bin-hadoop2.7 /opt/spark
-    source ~/.profile
+    echo "export SPARK_HOME=/opt/spark" >> ~/.bashrc
+    echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bashrc
+    echo "export PYSPARK_PYTHON=/usr/bin/python3" >> ~/.bashrc
+    source ~/.bashrc
     printf 'Spark setup complete. Try running start-master.sh\n'
 fi
-
-
 
